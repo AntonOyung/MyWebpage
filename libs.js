c0b3e008beb09339a15ec9e526e972524bf93a1c
Anton Oyung <antonoyung@Antons-MacBook-Pro.local>
2017-03-19 23:06:29 -0700

    update about page

--- libs.js
+++ libs.js
@@ -1,3 +1,12 @@
-$('.w3-btn').on('click', function(){
-  $(this).find('body').append('<div class="expand">HIHIHIHIHI</div>');
+// $('.w3-btn').on('click', function(){
+//   $(this).find('body').append('<div class="expand">HIHIHIHIHI</div>');
+// });
+
+
+$("#button").click(function() {
+  $(".expand").toggleClass('expand-active');
+  // document.getElementById("anton").innerHTML = "anton.oyung@berkeley.edu";
 });
+// $('.w3-btn').on('click', function(){
+//   $(this).find('body').toggleClass('expand-active');
+// });
