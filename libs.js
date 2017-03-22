9c9df304a573794de7d3263f7e4ddbbf79051ac5
Anton Oyung <antonoyung@Antons-MacBook-Pro.local>
2017-03-22 00:46:28 -0700

    fix formatting issue, implemented contact animation

--- libs.js
+++ libs.js
@@ -3,10 +3,34 @@
 // });
 
 
-$("#button").click(function() {
-  $(".expand").toggleClass('expand-active');
-  // document.getElementById("anton").innerHTML = "anton.oyung@berkeley.edu";
-});
+// $("#button").click(function() {
+//   $(".expand").toggleClass('expand-active');
+//   // document.getElementById("anton").innerHTML = "anton.oyung@berkeley.edu";
+// });
 // $('.w3-btn').on('click', function(){
 //   $(this).find('body').toggleClass('expand-active');
+// });
+
+// $('w3-btn').on("click", function() {
+//   var el = $(this);
+//   if (el.text() == el.data("anton.oyung")) {
+//     el.text(el.data("anton.oyung@berkeley.edu"));
+//   } else {
+//     el.data("anton.oyung@berkeley.edu", el.text());
+//     el.text(el.data("anton.oyung"));
+//   }
+// });
+
+// button = document.querySelectorAll("button")[0];
+// anton = document.querySelectorAll("b")[0];
+// button.addEventListener('click', function() {
+//     if ("anton.oyung" == anton.innerHTML) {
+//       anton.innerHTML = "anton.oyung@berkeley.edu";
+//     } else {
+//       anton.innerHTML = "anton.oyung"
+//     }
+// }, false);
+
+// $(function() {
+//     $('body').removeClass('fade-out');
 // });
